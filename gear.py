0303b4d5b0db17700c14bc74f5fd55d1be8214c5
ansunghae <ahshss903@gmail.com>
2023-05-03 08:22:32 +0900

    Files add

--- gear.py
+++ gear.py
@@ -1,21 +1,29 @@
 import requests
 import json
 
-city = "신내"
-url = "http://openapi.seoul.go.kr:8088/4e6643576861687339355165484770/json/GetParkInfo/1/5/{}".format(city)
-res = requests.get(url)
-res = res.text
-res = json.loads(res)
+lis = []
+parkingName = ""
+addr = ""
+tel = ""
 
-try :
-    for i in range(10):
-        parkingName = res["GetParkInfo"]["row"][i]["PARKING_NAME"]
-        addr = res["GetParkInfo"]["row"][i]["ADDR"]
-        tel = res["GetParkInfo"]["row"][i]["TEL"]
-        payNm = res["GetParkInfo"]["row"][i]["PAY_NM"]
+def search(search):
+    global lis
+    global parkingName
+    global addr
+    global tel
+    global cnt
 
-        print(parkingName,addr,tel,payNm)
-        print("---------- ----------")
-except:
-    pass
+    lis = []
 
+    url = "http://openapi.seoul.go.kr:8088/4e6643576861687339355165484770/json/GetParkInfo/1/5/%7B%7D%22.format(search)"
+    res = requests.get(url)
+    res = res.text
+    res = json.loads(res)
+    try :
+        for i in range(10):
+            parkingName = res["GetParkInfo"]["row"][i]["PARKING_NAME"]
+            addr = res["GetParkInfo"]["row"][i]["ADDR"]
+            tel = res["GetParkInfo"]["row"][i]["TEL"]
+            lis.append([parkingName,addr,tel])
+    except:
+        pass
